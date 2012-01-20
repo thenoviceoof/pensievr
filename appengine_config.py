b960ebe39d1545f1f8b12519f98d09ab69917dd3
Nathan Hwang <beta.entity.k@gmail.com>
2012-01-20 03:57:10 -0500

    Extended the session lifetime, prevent overlap with token lifetime

--- appengine_config.py
+++ appengine_config.py
@@ -3,5 +3,5 @@ import datetime
 
 def webapp_add_wsgi_middleware(app):
     app = SessionMiddleware(app, cookie_key="6353f9b2e7c47f73b48eef4544968bcd",
-                            lifetime=datetime.timedelta(hours=24))
+                            lifetime=datetime.timedelta(hours=48))
     return app

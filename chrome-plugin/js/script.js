d52261d766a97d627e8c6dcbb79d3e02c295d13f
Nathan Hwang <beta.entity.k@gmail.com>
2012-06-18 21:00:44 -0400

    structured out the ajax call, need to do serverside work

--- chrome-plugin/js/script.js
+++ chrome-plugin/js/script.js
@@ -1,7 +1,3 @@
-// auto-focus on the text field
-if(document.post_form)
-    document.post_form.post.focus();
-
 // geolocation
 function get_location() {
     function loc_success(position) {
@@ -21,6 +17,27 @@ function get_location() {
     }
 }
 
+var pensievr_url = "http://pensievr.appspot.com/";
+
 $(document).ready(function() {
     get_location();
+
+    // auto-focus on the text field
+    if(document.post_form)
+        document.post_form.post.focus();
+
+    $("#post_form").submit(function(e){
+        var d {
+            loc_lat: $("#loc_lat").val();
+            loc_long: $("#loc_lat").val();
+            time: $("#time").val();
+            post: $("#post").val();
+        };
+        $.ajax({
+            url: pensievr_url + "post",
+            data: d
+        });
+        e.preventDefault();
+        return false;
+    });
 }

a45a3361206b50942cc3bf381a3992f1d1256316
Nathan Hwang <thenoviceoof@gmail.com>
2012-06-20 22:55:11 -0400

    Just redirect to the pensievr page

---
+++ chrome-plugin/cs.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $(document).keydown(function(e) {
+        if(e.keyCode == 190 && e.ctrlKey)
+            window.location = "https://pensievr.appspot.com/"; 
+    });
+});

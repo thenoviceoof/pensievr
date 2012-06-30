024cc90bed669bba9096c10ea643907a905a1736
Nathan Hwang <thenoviceoof@gmail.com>
2012-06-30 02:14:44 -0400

    Have very simple proof of concept obfuscation per #1

--- static/js/script.js
+++ static/js/script.js
@@ -48,7 +48,37 @@ $(document).ready(function(){
         return false;
     });
 
+    // create a mapping
+    var m = new Array();
+    var uniq = new Array();
+    var r;
+    // !!! replace with fischer-yates shuffle
+    for(var i=0; i<256; i+=1) {
+        r = Math.floor(Math.random() * 256);
+        while(uniq[r]) {
+            r = Math.floor(Math.random() * 256);
+        }
+        m[i] = r;
+    }
+
+    var paper = Raphael(10, 10, 1000, 100);
+    var count = 0;
+    // display the contents with the mapping
+    function gen_char(num) {
+        paper.rect(count*30 - 2, -2, 24, 44, 4)
+            .attr({fill:"#DDD",stroke:"#DDD"});
+        for(var i=0; i<2; i++) {
+            for(var j=0; j<4; j++) {
+                if( (m[num] >> (i*4 + j)) & 1 == 1 ) {
+                    paper.rect(count*30 + i*10, j*10, 8,8).attr({fill:"black"});
+                }
+            }
+        }
+        count += 1;
+    }
+
     $("#post").keydown(function(e){
         // display stuff with raphael
+        gen_char(e.keyCode);
     });
 });
